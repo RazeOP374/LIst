981b944de4ec381cc9ebce4e56d552eeffe2ac81
SUISEI <491408600@qq.com>
2023-01-21 22:40:51 +0800

    no

--- memory_note/api/tasks.go
+++ memory_note/api/tasks.go
@@ -14,6 +14,27 @@ func CreateTask(c *gin.Context) {
 		res := createTask.Creat(chaim.Id)
 		c.JSON(200, res)
 	} else {
-		c.JSON(400, err)
+		c.JSON(400, ErrorResponse(err))
+	}
+}
+func ShowTask(c *gin.Context) {
+	var showTask service.ShowTaskService
+	err := c.ShouldBind(&showTask)
+	if err == nil {
+		res := showTask.Show(c.Param("id"))
+		c.JSON(200, res)
+	} else {
+		c.JSON(400, ErrorResponse(err))
+	}
+}
+func ListTask(c *gin.Context) {
+	var listTask service.ListTaskService
+	chaim, _ := utils.ParseToken(c.GetHeader("Authorization"))
+	err := c.ShouldBind(&listTask)
+	if err == nil {
+		res := listTask.List(chaim.Id)
+		c.JSON(200, res)
+	} else {
+		c.JSON(400, ErrorResponse(err))
 	}
 }
--- memory_note/service/task.go
+++ memory_note/service/task.go
@@ -6,22 +6,57 @@ import (
 	"time"
 )
 
+var code = 200
+
 type CreateTaskService struct {
 	Title   string `json:"title"  form:"title"`
 	Content string `json:"content" form:"content"`
 	Status  int    `json:"status"  form:"status"`
 }
+type ShowTaskService struct {
+}
+type ListTaskService struct {
+	PageNum  int `json:"page_num" form:"page_num"`
+	PageSize int `json:"page_size" form:"page_size"`
+}
 
-func (s *CreateTaskService) Creat(id uint) serializer.Response {
+func (l *ListTaskService) List(uid uint) serializer.Response {
+	var tasks []model.Task
+	count := 0
+	if l.PageSize == 0 {
+		l.PageSize = 15
+	}
+	model.DB.Model(model.Task{}).Preload("User").Where("uid = ?", uid).Count(&count).
+		Limit(l.PageSize).Offset((l.PageNum - 1) * l.PageSize).Find(&tasks)
+	return serializer.BuildListResponse(serializer.BuildTasks(tasks), uint(count))
+
+}
+func (s *ShowTaskService) Show(id string) serializer.Response {
+	var task model.Task
+
+	err := model.DB.First(&task, id).Error
+	if err != nil {
+		code = 500
+		return serializer.Response{
+			Status:  code,
+			Message: "查询失败",
+		}
+	}
+	return serializer.Response{
+		Status: code,
+		Data:   serializer.BuildTask(task),
+	}
+}
+
+func (c *CreateTaskService) Creat(id uint) serializer.Response {
 	var user model.User
-	code := 200
-	model.DB.First(&user)
+	model.DB.First(&user, id)
 	task := model.Task{
 		User:      user,
 		Uid:       user.ID,
-		Title:     s.Title,
+		Title:     c.Title,
 		Status:    0,
-		Content:   s.Content,
+		Content:   c.Content,
 		StartTime: time.Now().Unix(),
 	}
 	err := model.DB.Create(&task).Error
