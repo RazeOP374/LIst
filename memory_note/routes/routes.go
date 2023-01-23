1a5cb7d42c80bbf83a42c1a66784b494f93aa058
genshin impact <87218053+Raze9@users.noreply.github.com>
2023-01-23 19:14:02 +0800

    Update routes.go

--- memory_note/routes/routes.go
+++ memory_note/routes/routes.go
@@ -12,7 +12,7 @@ func NewRouter() *gin.Engine {
 	r := gin.Default()
 	store := cookie.NewStore([]byte("something-very-secret"))
 	r.Use(sessions.Sessions("mysession", store))
-	v1 := r.Group("api/v1")
+	v1 := r.Group("api")
 	{
 		v1.POST("user/register", api.UserRegister)
 		v1.POST("user/login", api.UserLogin)
@@ -21,7 +21,7 @@ func NewRouter() *gin.Engine {
 		{
 			authed.POST("task", api.CreateTask)
 			authed.GET("task/:id", api.ShowTask)
-			authed.GET("tasks", api.ListTask)
+			authed.GET("taskall", api.ListTask)
 			authed.PUT("task/:id", api.UpdateTask)
 			authed.POST("search", api.SearchTask)
 			authed.DELETE("task/:id", api.DeleteTask)
